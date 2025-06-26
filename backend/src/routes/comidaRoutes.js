3f29df1f3dad7a8ea5dd9e1f341437023032b52c
Daniela <danielarodrigues2po@gmail.com>
2025-06-26 20:39:16 -0300

    firebase + routers

---
+++ backend/src/routes/comidaRoutes.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/comidas', (req, res) => {
+
+
+});
+
+router.get('/comidas/:area', (req, res) => {
+
+
+});
+
+router.get('/comidas/:tipo', (req, res) => {
+
+    
+});
+
+router.get('/comidas/:ingrediente', (req, res) => {
+
+    
+});
+
+router.post('/comidas', (req, res) => {
+
+
+});
---
+++ backend/src/routes/usuarioRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/usuario/:id', (req, res) => {
+
+
+});
+
+router.get('/usario/:id/lista', (req, res) => {
+
+
+});
+
