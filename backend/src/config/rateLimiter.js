b38a7766bb1feaecd0aeb02317a49a1f841ca2e8
Daniela <danielarodrigues2po@gmail.com>
2025-07-02 16:38:29 -0300

    logger + rateLimiter

--- backend/src/config/rateLimiter.js
+++ backend/src/config/rateLimiter.js
@@ -1,4 +1,5 @@
 const rateLimit = require('express-rate-limit');
+const logger = require('./logger')
 
 const limiter = rateLimit({
     windowMs: 2 * 60 * 1000,
@@ -6,6 +7,12 @@ const limiter = rateLimit({
     message: 'Muitas requisições estão sendo feita para API_COMIDAS no momento tente novamente mais tarde por favor',
     standardHeaders: true,
     legacyHeaders: false,
+    handler: (req, res, next, options) => {
+        const ip = req.ip || 'Ip não foi encontrado';
+        logger.warn(`O rate limtier foi ultrapassado seu limite pelo seguinte ip: ${ip} na seguinte rota da api: ${req.originalUrl}`);
+        res.status(options.statusCode).json({ message: options.message});
+    }
+
 });
 
 module.exports = limiter;
