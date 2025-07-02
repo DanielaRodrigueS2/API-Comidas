cb76338ff7badffca487858a52963b0cded1528d
Daniela <danielarodrigues2po@gmail.com>
2025-07-02 16:15:48 -0300

    criacao logger

--- backend/src/config/loger.js
+++ backend/src/config/loger.js
@@ -4,11 +4,17 @@ const logger = createLogger({
     level: 'info',
     format: format.combine(
         format.timestamp(),
-        format.printf(() => {
-            
+        format.printf(({timestamp, level, message}) => {
+            return ` tempo: ${timestamp}  level: ${level.toUpperCase()} : ${message}`
         })
+    ),
 
-    )
+    transports:[
+        new transports.console(),
+        new transports.File({ filename: 'erros.log', level: 'error'}),
+        new transports.File({filename: 'todoslogs.log'})
+    ],
 
+});
 
-})
+module.exports = logger;
