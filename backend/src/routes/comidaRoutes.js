f57a0aeb029c981d61b7960ebba25fc55e94146f
Daniela <danielarodrigues2po@gmail.com>
2025-07-03 21:08:47 -0300

    validacao e proteção dos dados com express validator antes do envio para o banco de dados

--- backend/src/routes/comidaRoutes.js
+++ backend/src/routes/comidaRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const cache = require('express-redis-cache')() 
 const auth = require('../config/authMiddleware');
+const {body, validationQuery, validationResult} = require('express-validator')
 const {getAll, getById, getByArea, getByIngrediente, getByTipo, getAllAreas, getAllIngredientes, getAllTipos, create} = require('../models/comidaModel');
 
 
@@ -94,7 +95,25 @@ router.get('/comidas/:id', auth, async (req,res) =>{
     }
 });
 
-router.post('/comidas', auth, async(req, res) => {
+
+//Validacao de entradas, mais a protecao contra injections com escape
+router.post('/comidas', 
+    [
+        body('nome').trim().escape().isLength({min:1, max:60}),
+        body('area').trim().escape().isLength({min:1, max: 20}),
+        body('preparo').trim().escape().isLength({min:1, max:200}),
+        body('tipo').trim().escape().isLength({min:1, max: 20}),
+        body('ingrediente').isArray({min:1}),
+        body('ingrediente.*').trim().escape().isLength({min:1, max: 25})
+    ],
+    
+    auth, async(req, res) => {
+    
+    const erros = validationResult(req);
+
+    if(!erros.isEmpty()) return res.status(400).json({erros: erros});
+
+
     const dados = req.body;
     try{
         const comidas = await create(dados)
